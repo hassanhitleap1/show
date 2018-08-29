b22cdcb0c008593c74faabb9fd4913ce55dca756
hasan <hassanhitleap1@gmail.com>
2018-08-29 09:27:19 +0300

    ms

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        
+        $products=Product::paginate(10); 
         if($request->category=='home'){
             $products=Product::paginate(10); 
         }
