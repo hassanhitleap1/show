25960b612247d741b69502f493c4b61715b7b243
hasan kiwan <email@example.com>
2018-09-08 07:20:46 -0700

    tabe auth

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -32,23 +32,63 @@ $collection=Category::all();
                                     <a href="#"><i class="fa fa-user-o" aria-hidden="true"></i><sup></sup></a>
                                     <div class="mini-cart-content">
                                         <div class="contact-form">
-										    <h2 class="title18">login</h2>
-                                            <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                                                @csrf
-                                                <input id="email" type="email" placeholder="Email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-                                                 @if ($errors->has('email'))
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $errors->first('email') }}</strong>
-                                                    </span>
-                                                @endif
-                                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-                                                @if ($errors->has('password'))
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $errors->first('password') }}</strong>
-                                                    </span>
-                                                @endif
-                                                <input type="submit" value="login" class="shop-button" />
-                                            </form>
+                                            <div class="box-product box-has-filter">
+                                                <div class="title">
+                                                    <div class="row">
+                                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                                            <div class="box-tabs">
+                                                                <ul class="nav nav-tabs list-inline" role="tablist">
+                                                                    <li role="presentation" class="active"><h3><a data-toggle="tab" href="#tab1">login </a></h3></li>
+                                                                    <li role="presentation"><h3><a data-toggle="tab" href="#tab2">register</a></h3></li>							
+                                                                </ul>
+                                                            </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                                        <div class="tab-content">
+                                                            <div id="tab1" role="tabpanel" class="tab-pane fade in active">
+                                                                                    <h2 class="title18">login</h2>
+                                                                                <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                                                                                    @csrf
+                                                                                    <input id="email" type="email" placeholder="Email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                                                                    @if ($errors->has('email'))
+                                                                                        <span class="invalid-feedback" role="alert">
+                                                                                            <strong>{{ $errors->first('email') }}</strong>
+                                                                                        </span>
+                                                                                    @endif
+                                                                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                                                                    @if ($errors->has('password'))
+                                                                                        <span class="invalid-feedback" role="alert">
+                                                                                            <strong>{{ $errors->first('password') }}</strong>
+                                                                                        </span>
+                                                                                    @endif
+                                                                                    <input type="submit" value="login" class="shop-button" />
+                                                                                </form>
+                                                            </div>
+                                                            <div id="tab2" role="tabpanel" class="tab-pane fade in">
+                                                                                    <h2 class="title18">register</h2>
+                                                                                <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
+                                                                                    @csrf
+                                                                                    <input id="email" type="email" placeholder="Email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                                                                    @if ($errors->has('email'))
+                                                                                        <span class="invalid-feedback" role="alert">
+                                                                                            <strong>{{ $errors->first('email') }}</strong>
+                                                                                        </span>
+                                                                                    @endif
+                                                                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                                                                    @if ($errors->has('password'))
+                                                                                        <span class="invalid-feedback" role="alert">
+                                                                                            <strong>{{ $errors->first('password') }}</strong>
+                                                                                        </span>
+                                                                                    @endif
+                                                                                    <input type="submit" value="register" class="shop-button" />
+                                                                                </form>
+                                                                
+                                                            </div>
+                                                        </div>
+                                                        
+                                                    </div>
+										   
 									    </div>
                                     </div>
                                     @else
