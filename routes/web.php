e1741e32dc143d61d57fb603a895d30ceadcdb0a
hasan <hassanhitleap1@gmail.com>
2018-09-13 09:23:25 +0300

    admi login

--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,7 @@ Route::resource('/admin/slider','SliderController');
 Route::get('/admin/slider/{id}/delete-image','SliderController@deleteImage');
 Route::get('/admin/slider/{id}/publish', 'SliderController@publish');
 Route::get('/saved', 'HomeController@savedProduct')->name('savedProduct');
-// Route::get('/admin',function(){
-//     return view('auth.login');
-// });
+Route::get('/admin','AdminController@index');
 
 Route::get('/my-favorite', 'HomeController@myFavorite');
 
